c774fd0cd9e38a710f691faa77776a5275c30ac4
fspaniol <fspaniol@gmail.com>
2016-10-09 12:16:23 -0300

    Initial Commit

---
+++ SOP Otimizacao/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SOP Otimizacao
+//
+//  Created by Fernando Spaniol on 10/9/16.
+//  Copyright © 2016 Fernando Spaniol. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
