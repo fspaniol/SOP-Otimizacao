1a817e8bec71275b00340316df55668d92af293d
fspaniol <fspaniol@gmail.com>
2016-10-09 12:18:58 -0300

    Pegando dados do stdin

--- SOP Otimizacao/main.c
+++ SOP Otimizacao/main.c
@@ -2,14 +2,44 @@
 //  main.c
 //  SOP Otimizacao
 //
-//  Created by Fernando Spaniol on 10/9/16.
-//  Copyright © 2016 Fernando Spaniol. All rights reserved.
+//  Created by Fernando Spaniol and Isadora Possebon on 10/9/16.
+//  Copyright © 2016 Fernando Spaniol, Isadora Possebon. All rights reserved.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    printf("Começaremos: \n");
+    
+    char string[100];
+    int x, y;
+    int quantidade_cidades;
+    
+    for (x = 0; x < 7; x++){
+        gets(string);
+    }
+    
+    printf("Chegamos ao final dos requerimentos \n");
+    
+    scanf("%d", &quantidade_cidades); // LE DO STDIN QUANTAS CIDADES SERAO VISITADAS
+    
+    printf("Quantas cidades serão visitadas: %d \n", quantidade_cidades);
+    
+    int distancias[quantidade_cidades][quantidade_cidades];
+    
+    for(x = 0; x < quantidade_cidades; x++){
+        for(y = 0; y < quantidade_cidades; y++){
+            scanf("%d", &distancias[x][y]); // PREENCHE A MATRIZ
+        }
+    }
+    
+    for(x = 0; x < quantidade_cidades; x++){
+        for(y = 0; y < quantidade_cidades; y++){
+            printf("%d ", distancias[x][y]); // TESTA OS DADOS DA MATRIZ
+        }
+        printf("\n");
+    }
+    
+    gets(string); // TIRAR O EOF DO STDIN
     return 0;
 }
